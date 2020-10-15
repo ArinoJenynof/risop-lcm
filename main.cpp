bf7696e747befdfe4a748ed5186c4f2be2e7b4e5
Arino Jenynof <arino.j17@gmail.com>
2020-10-15 16:45:10 +0700

    Handle kasus cost minimum tidak unik
    Signed-off-by: Arino Jenynof <arino.j17@gmail.com>

--- main.cpp
+++ main.cpp
@@ -27,21 +27,60 @@ int main()
 		std::cin >> x;
 
 	// Cari cost terendah
-	int row, column;
 	int lowest = std::numeric_limits<int>::max();
 	for (size_t i = 0; i < cost_table.size(); i++)
 	{
 		for (size_t j = 0; j < cost_table.at(i).size(); j++)
 		{
-			if (!striked.at(i).at(j) && cost_table.at(i).at(j) < lowest)
+			if (!striked.at(i).at(j) && cost_table.at(i).at(j) <= lowest)
 			{
 				lowest = cost_table.at(i).at(j);
-				row = i;
-				column = j;
 			}
 		}
 	}
-	std::cout << lowest << " di (" << row << ", " << column << ")\n";
+
+	// Cari index cost terendah
+	std::vector<std::pair<int, int>> possibles;
+	for (size_t i = 0; i < cost_table.size(); i++)
+	{
+		for (size_t j = 0; j < cost_table.at(i).size(); j++)
+		{
+			if (!striked.at(i).at(j) && cost_table.at(i).at(j) == lowest)
+			{
+				possibles.push_back(std::make_pair(i, j));
+			}
+		}
+	}
+
+	// Test masing-masing untuk alokasi tertinggi
+	int highest_alloc = 0;
+	int row, column, curr;
+	for (const auto &x : possibles)
+	{
+		curr = std::min(supply[x.first], demand[x.second]);
+		std::cout << "(" << x.first << ", " << x.second << ") dengan alokasi " << curr << '\n';
+		if (curr > highest_alloc)
+		{
+			highest_alloc = curr;
+			row = x.first;
+			column = x.second;
+		}
+	}
+	std::cout << "yang dipilih (" << row << ", " << column << ") dengan alokasi " << highest_alloc << '\n';
+
+	// Kurangi supply dan demand
+	int minimum = std::min(supply[row], demand[column]);
+	supply[row] -= minimum;
+	demand[column] -= minimum;
+
+	// Coret jika sudah 0
+	if (supply[row] == 0)
+		for (auto it = striked.at(row).begin(); it != striked.at(row).end(); it++)
+			*it = true;
+
+	if (demand[column] == 0)
+		for (auto it = striked.begin(); it != striked.end(); it++)
+			it->at(column) = true;
 	
 	return 0;
 }
