d406a246eb8ac0f92111af0749c5131862724f05
Arino Jenynof <arino.j17@gmail.com>
2020-10-16 19:08:34 +0700

    Refactor, clear answer setiap iterasi soal
    Signed-off-by: Arino Jenynof <arino.j17@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,102 +3,126 @@
 #include <algorithm>
 #include <limits>
 #include <random>
+#include <string>
+#include <fstream>
+#include <chrono>
+#include <dirent.h>
 
 bool finished(std::vector<std::vector<bool>> &striked);
 void print_table(
 	std::vector<std::vector<int>> &cost_table,
 	std::vector<std::vector<bool>> &striked,
 	std::vector<int> &supply,
-	std::vector<int> &demand
+	std::vector<int> &demand,
+	std::ofstream &out,
+	int &counter
 );
 
 int main()
 {
-	int rows, columns;
-	std::cin >> rows;
-	std::cin >> columns;
-
-	// Forward declaration
-	std::vector<std::vector<int>> cost_table(rows, std::vector<int>(columns));
-	std::vector<std::vector<bool>> striked(rows, std::vector<bool>(columns));
-	int lowest;
-	std::vector<std::pair<int, int>> possibles;
-	std::pair<int, int> chosen;
-	int highest_alloc, row, column, curr;
-	std::vector<std::pair<int, int>> answer;
-
 	// PRNG
 	std::random_device rd;
 	std::mt19937_64 mt(rd());
-
-	// Isi tabel dengan cost
-	for (auto &row : cost_table)
-		for (auto &column : row)
-			std::cin >> column;
-
-	std::vector<int> supply(rows);
-	std::vector<int> demand(columns);
-
-	// Isi supply dan demand
-	for (auto &x : supply)
-		std::cin >> x;
-	for (auto &x : demand)
-		std::cin >> x;
-
-	print_table(cost_table, striked, supply, demand);
-	while (!finished(striked))
-	{
-		// Cari cost terendah
-		lowest = std::numeric_limits<int>::max();
-		for (size_t i = 0; i < cost_table.size(); i++)
-			for (size_t j = 0; j < cost_table.at(i).size(); j++)
-				if (!striked.at(i).at(j) && cost_table.at(i).at(j) <= lowest)
-					lowest = cost_table.at(i).at(j);
-
-		// Cari index cost terendah
-		possibles.clear();
-		for (size_t i = 0; i < cost_table.size(); i++)
-			for (size_t j = 0; j < cost_table.at(i).size(); j++)
-				if (!striked.at(i).at(j) && cost_table.at(i).at(j) == lowest)
-					possibles.push_back(std::make_pair(i, j));
-
-		// Jika cost minimum tidak unik, pilih secara random mana yang dipakai
-		if (possibles.size() > 1)
-		{
-			std::uniform_int_distribution<> distrib(0, possibles.size() - 1);
-			chosen = possibles.at(distrib(mt));			
-		}
-		else
-		{
-			chosen = possibles.at(0);
-		}
-		highest_alloc = std::min(supply.at(chosen.first), demand.at(chosen.second));
-		row = chosen.first;
-		column = chosen.second;
-
-		// Kurangi supply dan demand
-		supply[row] -= highest_alloc;
-		demand[column] -= highest_alloc;
-
-		// Coret jika sudah 0
-		if (supply[row] == 0)
-			for (auto it = striked.at(row).begin(); it != striked.at(row).end(); it++)
-				*it = true;
-
-		if (demand[column] == 0)
-			for (auto it = striked.begin(); it != striked.end(); it++)
-				it->at(column) = true;
-
-		// Masukkan ke penyelesaian
-		answer.push_back(std::make_pair(cost_table.at(row).at(column), highest_alloc));
-		print_table(cost_table, striked, supply, demand);
-	}
-
-	int total = 0;
-	for (const std::pair<int, int> &i : answer)
-		total += (i.first * i.second);
-	std::cout << total << '\n';
 	
+	const std::string soal = ".\\soal";
+	struct dirent *entry;
+	DIR *dp = opendir(soal.c_str());
+	while ((entry = readdir(dp)))
+	{
+		int counter = 0;
+		auto begin = std::chrono::steady_clock::now();
+		std::string nama(entry->d_name);
+		if (nama == "." || nama == "..")
+			continue;
+		std::ifstream inp(soal + "\\" + nama);
+		std::ofstream out(".\\jawab\\" + nama);
+
+		// Baris = supply, kolom = demand
+		int rows, columns;
+		inp >> rows;
+		inp >> columns;
+		
+		std::vector<std::vector<int>> cost_table(rows, std::vector<int>(columns));
+		std::vector<std::vector<bool>> striked(rows, std::vector<bool>(columns));
+		std::vector<std::pair<int, int>> answer(0);
+
+		// Isi tabel dengan cost
+		for (auto &row : cost_table)
+			for (auto &column : row)
+				inp >> column;
+
+		std::vector<int> supply(rows);
+		std::vector<int> demand(columns);
+
+		// Isi supply dan demand
+		for (auto &x : supply)
+			inp >> x;
+		for (auto &x : demand)
+			inp >> x;
+
+		print_table(cost_table, striked, supply, demand, out, counter);
+		while (!finished(striked))
+		{
+			// Cari cost terendah
+			int lowest = std::numeric_limits<int>::max();
+			for (size_t i = 0; i < cost_table.size(); i++)
+				for (size_t j = 0; j < cost_table.at(i).size(); j++)
+					if (!striked.at(i).at(j) && cost_table.at(i).at(j) <= lowest)
+						lowest = cost_table.at(i).at(j);
+
+			// Cari index cost terendah
+			std::vector<std::pair<int, int>> possibles(0);
+			for (size_t i = 0; i < cost_table.size(); i++)
+				for (size_t j = 0; j < cost_table.at(i).size(); j++)
+					if (!striked.at(i).at(j) && cost_table.at(i).at(j) == lowest)
+						possibles.push_back(std::make_pair(i, j));
+
+			// Jika cost minimum tidak unik, pilih secara random mana yang dipakai
+			std::pair<int, int> chosen;
+			if (possibles.size() > 1)
+			{
+				std::uniform_int_distribution<> distrib(0, possibles.size() - 1);
+				chosen = possibles.at(distrib(mt));			
+			}
+			else
+			{
+				chosen = possibles.at(0);
+			}
+			int allocation = std::min(supply.at(chosen.first), demand.at(chosen.second));
+			int row = chosen.first;
+			int column = chosen.second;
+
+			// Kurangi supply dan demand
+			supply.at(row) -= allocation;
+			demand.at(column) -= allocation;
+
+			// Coret jika sudah 0
+			if (supply.at(row) == 0)
+				for (auto it = striked.at(row).begin(); it != striked.at(row).end(); it++)
+					*it = true;
+
+			if (demand.at(column) == 0)
+				for (auto it = striked.begin(); it != striked.end(); it++)
+					it->at(column) = true;
+
+			// Masukkan ke penyelesaian
+			answer.push_back(std::make_pair(cost_table.at(row).at(column), allocation));
+			print_table(cost_table, striked, supply, demand, out, ++counter);
+		}
+
+		int total = 0;
+		for (const std::pair<int, int> &i : answer)
+			total += (i.first * i.second);
+		out << "Solusi: " << total << '\n';
+		answer.clear();
+
+		auto end = std::chrono::steady_clock::now();
+		auto elapsed = end - begin;
+		out << "Runtime: " << std::chrono::duration<float, std::milli>(elapsed).count() << "ms\n";
+		std::cout << "Runtime: " << std::chrono::duration<float, std::milli>(elapsed).count() << "ms\n";
+	}
+	closedir(dp);
+
 	return 0;
 }
 
@@ -116,21 +140,25 @@ void print_table(
 	std::vector<std::vector<int>> &cost_table,
 	std::vector<std::vector<bool>> &striked,
 	std::vector<int> &supply,
-	std::vector<int> &demand
+	std::vector<int> &demand,
+	std::ofstream &out,
+	int &counter
 ){
+	if (counter > 0)
+		out << "Iterasi ke-" << counter << '\n';
 	for (auto i = 0; i < cost_table.size(); i++)
 	{
 		for (auto j = 0; j < cost_table.at(i).size(); j++)
 		{
 			if (striked.at(i).at(j))
-				std::cout << "x\t";
+				out << "x\t";
 			else
-				std::cout << cost_table.at(i).at(j) << '\t';
+				out << cost_table.at(i).at(j) << '\t';
 		}
-		std::cout << supply.at(i) << '\n';
+		out << supply.at(i) << '\n';
 	}
 
 	for (auto it = demand.begin(); it != demand.end(); it++)
-		std::cout << *it << '\t';
-	std::cout << "\n\n";
+		out << *it << '\t';
+	out << "\n\n";
 }
